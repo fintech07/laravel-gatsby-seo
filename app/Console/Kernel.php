b2cc2347c6588f5766c0c89fc25f4e2172bf052e
Robert <funnydeveloper100@gmail.com>
2019-05-13 07:20:21 +1000

    daily scheduled

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('SitemapUpdater:check')
-            ->everyMinute();
+            //->everyMinute();
+            ->daily();
     }
 
     /**

b71d0db35a2a30584f56942994bc74286af661f0
Robert <funnydeveloper100@gmail.com>
2019-05-16 04:30:29 +1000

    remove the gatsby plugin offlin

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,6 @@ module.exports = {
                 //trackingId: `ADD YOUR TRACKING ID HERE`,
             }
         },
-        `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-plugin-typography`,
